ade67dd0a248f64058e92a0467b23136bf9f6f4d
Pavka <null@null.equestria>
2026-07-31 15:30:06 +0700

    unbind <C-w> for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,13 +13,18 @@ config.keys = {
     mods = 'ALT',
     action = wezterm.action.ToggleFullScreen,
     },
-    { key = 'v', 
-    mods = 'ALT', 
-    action = wezterm.action.SplitHorizontal{ domain = 'CurrentPaneDomain' } 
+    { key = 'v',
+    mods = 'ALT',
+    action = wezterm.action.SplitHorizontal{ domain = 'CurrentPaneDomain' }
     },
-    { key = 'h', 
-    mods = 'ALT', 
-    action = wezterm.action.SplitVertical{ domain = 'CurrentPaneDomain' } 
+    { key = 'h',
+    mods = 'ALT',
+    action = wezterm.action.SplitVertical{ domain = 'CurrentPaneDomain' }
+    },
+    {
+      key = 'w',
+      mods = 'CTRL',
+      action = wezterm.action.DisableDefaultAssignment,
     },
 }
 return config
