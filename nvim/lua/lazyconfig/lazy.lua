a1297471b9fe1befc3388ea96bf68564a4421976
Pavka <null@null.equestria>
2026-08-27 16:09:03 +0200

    disable auto-detection of plugin updates(lazy)

--- nvim/lua/lazyconfig/lazy.lua
+++ nvim/lua/lazyconfig/lazy.lua
@@ -28,8 +28,8 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "twilight-sparkle-colorscheme" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  -- do not automatically check for plugin updates
+  checker = { enabled = false },
 })
 
 -- ==============================================================================
